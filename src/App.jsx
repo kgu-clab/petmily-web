da23c96bb64345bcf5b9881c6df0110631196833
gwansikk <seorit98@gmail.com>
2023-08-12 00:36:39 +0900

    feat: Add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,39 @@
-import Sidebar from './components/Sidebar';
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Sidebar from '@components/Sidebar';
+import Home from '@pages/Home';
+import Root from '@pages/Root';
 
-function App() {
+const Section = () => {
   return (
-    <section className="flex">
+    <section>
       <Sidebar />
+
+      <div className="ml-[18rem] grow p-5">
+        <Outlet />
+      </div>
     </section>
   );
-}
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Section />,
+    children: [
+      {
+        path: '',
+        element: <Root />,
+      },
+      {
+        path: 'home',
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
