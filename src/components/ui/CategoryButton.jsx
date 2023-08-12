66283a83cd8c3021b2f6da008a966bdcf7092d9e
gwansikk <seorit98@gmail.com>
2023-08-12 23:08:30 +0900

    refactor: CategoryButton onClick, checked

--- src/components/ui/CategoryButton.jsx
+++ src/components/ui/CategoryButton.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 
-const CategoryButton = ({ icon, label }) => {
+const CategoryButton = ({ icon, label, checked, onClick }) => {
   return (
-    <button className="group flex w-16 flex-col items-center">
-      <div className="flex h-10 w-full items-center justify-center rounded-lg border-2 border-gray-300 bg-gray-300 hover:border-pm-main group-hover:border-pm-main">
-        <img src={icon} alt="강아지" className="h-5 w-5" />
+    <button className="group flex w-16 flex-col items-center" onClick={onClick}>
+      <div
+        className={`flex h-10 w-full items-center justify-center rounded-lg border-2 bg-gray-300 hover:border-pm-main group-hover:border-pm-main ${
+          checked ? 'border-pm-main' : 'border-gray-300'
+        }`}
+      >
+        <img src={icon} alt="동물" className="h-7 w-7" />
       </div>
 
-      <h2 className="mt-1 text-sm font-medium text-gray-800">{label}</h2>
+      <h2
+        className={`mt-1 text-xs text-gray-800 ${
+          checked ? 'font-bold' : 'font-medium'
+        }`}
+      >
+        {label}
+      </h2>
     </button>
   );
 };
@@ -15,6 +25,8 @@ const CategoryButton = ({ icon, label }) => {
 CategoryButton.propTypes = {
   icon: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
+  checked: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default CategoryButton;
