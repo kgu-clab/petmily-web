e7769c47c36202adc352109ecc68ef35129d670c
gwansikk <seorit98@gmail.com>
2023-08-12 20:56:50 +0900

    refactor: Footer 내용 변경

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,27 @@
 const Footer = () => {
   return (
     <footer>
-      <hr className="border-gray-500" />
-
       <ul className="mt-2 text-center text-xs font-medium text-gray-500">
-        <li>제 10회 SW개발보안 경진대회 | 소개딩 시즌5</li>
+        <li>
+          <a
+            href="https://securecoding.software/"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:underline"
+          >
+            제 10회 SW개발보안 경진대회 | 소개딩 시즌5
+          </a>
+        </li>
+        <li>C-Lab / 이석현 / 김관식 / 한관희 / 김정은</li>
         <li className="text-sm">
-          Copyright © 2023 C-Lab. All rights reserved.
+          <a
+            href="https://github.com/KGU-C-Lab"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:underline"
+          >
+            Copyright © 2023 C-Lab. All rights reserved.
+          </a>
         </li>
       </ul>
     </footer>
