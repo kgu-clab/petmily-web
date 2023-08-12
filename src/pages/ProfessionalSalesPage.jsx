d816529e682b40983033e575908b16c5e941ef55
gwansikk <seorit98@gmail.com>
2023-08-12 19:45:50 +0900

    refactor: ProfessionalSalesPage

--- src/pages/ProfessionalSalesPage.jsx
+++ src/pages/ProfessionalSalesPage.jsx
@@ -1,7 +1,24 @@
-import PropTypes from 'prop-types';
+import DogIcon from '@assets/dog.svg';
+import CategoryButton from '@components/ui/CategoryButton';
+
+const animals = [
+  '전체',
+  '강아지',
+  '고양이',
+  '새',
+  '물고기',
+  '소형 동물',
+  '파충류',
+];
 
 const ProfessionalSalesPage = () => {
-  return <div></div>;
+  return (
+    <div className="flex gap-6">
+      {animals.map((item) => (
+        <CategoryButton key={item} icon={DogIcon} label={item} />
+      ))}
+    </div>
+  );
 };
 
 ProfessionalSalesPage.propTypes = {};
