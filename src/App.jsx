342fe3e18b61b063b04d76ca82aa7ed8db11d9ad
gwansikk <seorit98@gmail.com>
2023-08-13 22:10:20 +0900

    hotfix: 라우팅 재설정

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,8 @@ const router = createBrowserRouter([
       {
         path: '/list',
         element: <AnimalListPage />,
+      },
+      {
         path: '/login',
         element: <LoginPage />,
       },
