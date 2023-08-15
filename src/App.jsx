fed2503ad581b4fe95b19a3bd0982f26e67c71fa
gwansikk <seorit98@gmail.com>
2023-08-16 03:18:06 +0900

    refactor: Mypage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import AnimalPage from '@pages/AnimalPage';
 import AnimalListPage from '@pages/AnimalListPage';
 import LoginPage from '@pages/LoginPage';
 import AnimalRegistrationPage from '@pages/AnimalRegistrationPage';
+import MyPage from '@pages/MyPage';
 
 const Section = () => {
   return (
@@ -49,6 +50,10 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginPage />,
+      },
+      {
+        path: '/mypage',
+        element: <MyPage />,
       },
     ],
   },
