16a4ee8720bcc24b0d28d5cc0cf09435d2142c7d
gwansikk <seorit98@gmail.com>
2023-08-16 23:08:53 +0900

    feat: 회원가입 페이지 API

--- src/common/api.js
+++ src/common/api.js
@@ -4,3 +4,18 @@ export const postLogin = async (payload) => {
   const res = await server.post(`/login`, payload);
   return res.data.data;
 };
+
+export const postUsers = async (payload) => {
+  const res = await server.post(`/users`, payload);
+  return res.data.data;
+};
+
+export const postSendVerificationCode = async (payload) => {
+  const res = await server.post(`/sms/send-verification-code`, payload);
+  return res.data.data;
+};
+
+export const postVerificationCode = async (payload) => {
+  const res = await server.post(`/sms/verify`, payload);
+  return res.data.data;
+};
