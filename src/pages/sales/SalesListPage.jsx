fec0e8c8a8ddebaf4e774a91073f0b9479a34bc9
gwansikk <seorit98@gmail.com>
2023-08-18 00:45:59 +0900

    refactor: 컴포넌트 최적화

--- src/pages/sales/SalesListPage.jsx
+++ src/pages/sales/SalesListPage.jsx
@@ -8,41 +8,22 @@ const SalesListPage = () => {
       <AnimalSection
         title="전문업체분양"
         sub="팻밀리가 검증한 전문업체 분양입니다."
-        to="/professional"
-      >
-        <AnimalCard
-          title="시고르자브종"
-          provider="한관희전문분양"
-          price={100000}
-          to="/animal/1"
-        />
-      </AnimalSection>
+        to="/sales/professional"
+      ></AnimalSection>
 
       {/* 보호소분양 */}
       <AnimalSection
         title="보호소분양"
         sub="보호소에서 보호하고 있는 반려동물입니다."
-        to="/shelter"
-      >
-        <AnimalCard
-          title="시고르자브종"
-          provider="김정은보호소"
-          price={100000}
-        />
-      </AnimalSection>
+        to="/sales/shelter"
+      ></AnimalSection>
 
       {/* 개인무료분양 */}
       <AnimalSection
         title="개인무료분양"
         sub="개인 가정의 무료 분양입니다."
-        to="/personal"
-      >
-        <AnimalCard
-          title="시고르자브종"
-          provider="팻밀리관리자"
-          price={100000}
-        />
-      </AnimalSection>
+        to="/sales/individual"
+      ></AnimalSection>
     </div>
   );
 };
